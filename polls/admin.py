c2ea0f1f348a6ff619ab0ddae67bea797eed47c5
thinkAmi <dev.thinkami+git@gmail.com>
2015-11-18 23:14:39 +0900

    Adding related objects

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 # カスタマイズ無し
 # admin.site.register(Question)
@@ -24,4 +24,29 @@ class QuestionAdmin3(admin.ModelAdmin):
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-admin.site.register(Question, QuestionAdmin3)
+# admin.site.register(Question, QuestionAdmin3)
+
+
+# Choice model の画面への追加
+# admin.site.register(Choice)
+
+# QuestionとChoiceの登録を一括で行う
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+# table-baseなフォーマットの場合
+class ChoiceInline2(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin4(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    # inlines = [ChoiceInline] #標準的な一括登録
+    inlines = [ChoiceInline2]   # テーブル的な表現のコンパクトな一括登録
+
+
+admin.site.register(Question, QuestionAdmin4)
