a9390d0003f14c34959358921b70558c1b308548
thinkAmi <dev.thinkami+git@gmail.com>
2015-11-25 00:29:07 +0900

    Use generic views: Less code is better

--- polls/views.py
+++ polls/views.py
@@ -3,45 +3,66 @@ from django.template import RequestContext, loader
 from django.http import Http404
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    # Response:
-    # - full
-    # return HttpResponse(template.render(context))
-    # - shortcut
-    return render(request, 'polls/index.html', context)
+# generic view class
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    # return HttpResponse("Hello, world. You're at the polls index.")
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+# non-generic view class
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = RequestContext(request, {
+#         'latest_question_list': latest_question_list,
+#     })
+#     # Response:
+#     # - full
+#     # return HttpResponse(template.render(context))
+#     # - shortcut
+#     return render(request, 'polls/index.html', context)
+#
+#     # return HttpResponse("Hello, world. You're at the polls index.")
 
-def detail(request, question_id):
+# generic view
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+# non-generic view
+# def detail(request, question_id):
+#
+#     # Response:
+#     # - full
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'polls/detail.html', {'question': question})
+#     #
+#     # - shortcut
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+#
+#     # return HttpResponse("You're looking at question %s." % question_id)
 
-    # Response:
-    # - full
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, 'polls/detail.html', {'question': question})
-    #
-    # - shortcut
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
-    # return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-    # response = HttpResponse("You're looking at the results of question %s.")
-    # return HttpResponse(response % question_id)
+# generic view
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+# non-generic view
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+#
+#     # response = HttpResponse("You're looking at the results of question %s.")
+#     # return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
