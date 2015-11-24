09fe47e948c1f51f40fa9f799523f70a28286816
thinkAmi <dev.thinkami+git@gmail.com>
2015-11-24 23:10:34 +0900

    Removing hardcoded URLs in templates

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # /polls/
     url(r'^$', views.index, name='index'),
     # /polls/5/
+    # nameの値は、{% url %} テンプレートタグで呼び出される
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
