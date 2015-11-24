3a8a33b1ecec3e979e7215f1eb7a851320b45080
thinkAmi <dev.thinkami+git@gmail.com>
2015-11-24 23:40:14 +0900

    Write a simple form

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.http import Http404
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -36,8 +37,24 @@ def detail(request, question_id):
     # return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = HttpResponse("You're looking at the results of question %s.")
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+    # response = HttpResponse("You're looking at the results of question %s.")
+    # return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+    # return HttpResponse("You're voting on question %s." % question_id)
